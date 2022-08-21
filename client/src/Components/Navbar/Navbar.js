beb3fd1e0b85a453770a44c11c0fa05ec6814406
Yash Sinha <yashsinha4909@gmail.com>
2022-08-22 00:41:46 +0530

    navbar

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <>
-    <div className='navbar'>
+    <div className = 'navbar'>
       <div className='title-div'>
         <h3>Virtual Meet and Code</h3>
       </div>
